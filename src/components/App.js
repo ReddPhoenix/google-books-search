08cbd0666b6fcd4349f9adbb798f4b3b8464abbe
Walter Guerrero <wsguerrero@gmail.com>
2021-02-09 00:21:04 -0700

    created routes to Home & SavedBooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,21 @@
 import React, { Component } from 'react'
-import Router
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Home from './Home/Home.jsx';
+import SavedBooks from './SavedBooks/SavedBooks';
+
+class App extends Component {
+    render() {
+        return (
+            
+            <BrowserRouter>
+                
+                <Route exact path='/' component={Home} />
+                <Route path='/saved' component={SavedBooks} />
+            
+            </BrowserRouter>
+        )
+    }
+}
+
+export default App;
